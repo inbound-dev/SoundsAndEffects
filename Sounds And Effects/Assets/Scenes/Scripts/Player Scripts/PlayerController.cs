513df0112d2743ae35af4fba84055d0c6d626767
Jordan Hamilton <jordanh6489@hotmail.com>
2021-10-13 13:17:45 -0400

    Added Audio
    Audio added for crashing, jumping, and general background music

--- Sounds And Effects/Assets/Scenes/Scripts/Player Scripts/PlayerController.cs
+++ Sounds And Effects/Assets/Scenes/Scripts/Player Scripts/PlayerController.cs
@@ -15,11 +15,19 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
 
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip crashSound;
+    public AudioClip jumpSound;
+
+    private AudioSource playerAudio;
 
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+
         Physics.gravity *= GravityModifier;
     }
 
@@ -27,23 +35,33 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) && isOnGround == true && !GameOver){
             PlayerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+
             isOnGround = false;
+
             playerAnim.SetTrigger("Jump_trig");
+
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if(collision.gameObject.CompareTag("Obstacle")){
             GameOver = true;
             Debug.Log("Game Over!");
-            
+
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
 
             explosionParticle.Play();
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
